208be5fa5d39dd7671e912ea53d1effd5db4ae22
unknown <pohl.daniel@freenet.de>
2024-11-21 14:43:01 +0100

    preload

--- src/assets/wasm/open/build.bat
+++ src/assets/wasm/open/build.bat
@@ -1,3 +1,3 @@
-emcc src/Application.cpp src/glError.cpp src/main.cpp src/MyApplication.cpp src/Shader.cpp -o open.js -Wall -std=c++14 -Wno-missing-braces -Wunused-result -Os -I include -I "C:/OpenGL/lib/glm" --use-port=contrib.glfw3 -s USE_WEBGL2=1 -s MIN_WEBGL_VERSION=2 --embed-file shader -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s "EXPORTED_FUNCTIONS=['_free','_malloc','_main']" -s "EXPORTED_RUNTIME_METHODS=['ccall']" -s MODULARIZE=1 -s EXPORT_NAME='OpenModule'
+emcc src/Application.cpp src/glError.cpp src/main.cpp src/MyApplication.cpp src/Shader.cpp -o open.js -Wall -std=c++14 -Wno-missing-braces -Wunused-result -Os -I include -I "C:/OpenGL/lib/glm" --use-port=contrib.glfw3 -s USE_WEBGL2=1 -s MIN_WEBGL_VERSION=2 --preload-file shader -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s "EXPORTED_FUNCTIONS=['_free','_malloc','_main']" -s "EXPORTED_RUNTIME_METHODS=['ccall']" -s MODULARIZE=1 -s EXPORT_NAME='OpenModule'
 
 cmd /k
