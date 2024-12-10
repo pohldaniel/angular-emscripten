8c8e7a718c0eb79a66afb71c382d7d88c872d7cc
unknown <pohl.daniel@freenet.de>
2024-12-10 16:15:55 +0100

    entrypoint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,11 +8,7 @@ import {WebGPUComponent} from './pages/web-gpu/web-gpu.component';
 import {ShapeComponent} from './pages/shape/shape.component';
 
 export const routes: Routes = [
-  { path: 'imgui', 
-    component: ImguiComponent,
-    canActivate: [authGuard],
-    data : {showSidebar: true},
-  },  
+  
   { path: 'open-gl', 
     component: OpenGLComponent,
     canActivate: [authGuard],
