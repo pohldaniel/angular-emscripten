8ea223b4d2ad3582459d7ae22ada7aa735498ce1
Daniel Pohl <pohl.daniel@freenet.de>
2026-05-14 00:45:45 +0200

    prepare compute

--- src/assets/wasm/12ImageBasedLighting/WebGPU/WgpContext.cpp
+++ src/assets/wasm/12ImageBasedLighting/WebGPU/WgpContext.cpp
@@ -766,7 +766,7 @@ void WgpContext::addSampler(const WGPUSampler& sampler, SamplerSlot samplerSlot)
 		samplers[samplerSlot] = sampler;
 }
 
-const WGPUSampler& WgpContext::getSampler(SamplerSlot samplerSlot) {
+const WGPUSampler& WgpContext::getSampler(SamplerSlot samplerSlot) const {
 	return samplers.at(samplerSlot);
 }
 
@@ -774,27 +774,35 @@ void WgpContext::addSahderModule(const std::string& shaderModuleName, const std:
 	shaderModules[shaderModuleName] = fromString ? wgpCreateShaderFromString(stringPath) : wgpCreateShaderFromFile(stringPath);
 }
 
-const WGPUShaderModule& WgpContext::getShaderModule(std::string shaderModuleName) {
+const WGPUShaderModule& WgpContext::getShaderModule(std::string shaderModuleName) const {
 	return shaderModules.at(shaderModuleName);
+}
+
+const WGPUPipelineLayout& WgpContext::getPipelineLayout(std::string pipelineLayoutName) const {
+	return pipelineLayouts.at(pipelineLayoutName);
 }
 
 void WgpContext::setClearColor(const WGPUColor& _clearColor) {
 	clearColor = _clearColor;
 }
 
-void WgpContext::createComputePipeline(std::string shaderModuleName, std::string pipelineLayoutName, const std::function<std::vector<WGPUBindGroupLayout>()>& onBindGroupLayouts) {
-	std::vector<WGPUBindGroupLayout> bindGroupLayouts = onBindGroupLayouts();
+void WgpContext::createComputePipeline(std::string shaderModuleName, 
+	                                   std::string entrypoint,
+	                                   std::string pipelineLayoutName, 
+	                                   const std::function<std::vector<WGPUBindGroupLayout>()>& onBindGroupLayouts) {
 
-	WGPUPipelineLayoutDescriptor pipelineLayoutDescriptor = {};
-	pipelineLayoutDescriptor.bindGroupLayoutCount = bindGroupLayouts.size();
-	pipelineLayoutDescriptor.bindGroupLayouts = bindGroupLayouts.data();
-	pipelineLayouts[pipelineLayoutName] = wgpuDeviceCreatePipelineLayout(device, &pipelineLayoutDescriptor);
+	if (onBindGroupLayouts) {
+		std::vector<WGPUBindGroupLayout> bindGroupLayouts = onBindGroupLayouts();
+		WGPUPipelineLayoutDescriptor pipelineLayoutDescriptor = {};
+		pipelineLayoutDescriptor.bindGroupLayoutCount = bindGroupLayouts.size();
+		pipelineLayoutDescriptor.bindGroupLayouts = bindGroupLayouts.data();
+		pipelineLayouts[pipelineLayoutName] = wgpuDeviceCreatePipelineLayout(device, &pipelineLayoutDescriptor);
+	}
 
 	WGPUComputePipelineDescriptor computePipelineDesc = {};
-	computePipelineDesc.layout = pipelineLayouts.at(pipelineLayoutName);
+	computePipelineDesc.layout = onBindGroupLayouts ? pipelineLayouts.at(pipelineLayoutName) : NULL;
 	computePipelineDesc.compute.module = shaderModules.at(shaderModuleName);
-	//computePipelineDesc.compute.entryPoint = "computeFilter";
-	computePipelineDesc.compute.entryPoint = "computeSobelX";
+	computePipelineDesc.compute.entryPoint = entrypoint.c_str();
 	computePipelineDesc.compute.constantCount = 0u;
 	computePipelineDesc.compute.constants = NULL;
 	
@@ -846,9 +854,9 @@ void WgpContext::createRenderPipeline(std::string shaderModuleName,
 	WGPUFragmentState fragmentState = {};
 	fragmentState.module = shaderModules.at(shaderModuleName);
 	fragmentState.entryPoint = "fs_main";
-	fragmentState.constantCount = 0;
+	fragmentState.constantCount = 0u;
 	fragmentState.constants = NULL;
-	fragmentState.targetCount = 1;
+	fragmentState.targetCount = 1u;
 	fragmentState.targets = &colorTarget;
 
 	WGPUDepthStencilState depthStencilState = {};
@@ -856,8 +864,8 @@ void WgpContext::createRenderPipeline(std::string shaderModuleName,
 	depthStencilState.depthCompare = depthCompareFunction;
 	depthStencilState.depthWriteEnabled = true;
 	depthStencilState.format = depthTextureFormat == WGPUTextureFormat_Undefined ? depthformat : depthTextureFormat;
-	depthStencilState.stencilReadMask = 0;
-	depthStencilState.stencilWriteMask = 0;
+	depthStencilState.stencilReadMask = 0u;
+	depthStencilState.stencilWriteMask = 0u;
 
 	WGPURenderPipelineDescriptor renderPipelineDescriptor = {};
 	renderPipelineDescriptor.layout = onBindGroupLayouts ? pipelineLayouts.at(pipelineLayoutName) : NULL;
