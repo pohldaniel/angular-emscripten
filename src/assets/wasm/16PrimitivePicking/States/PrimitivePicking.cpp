13a644061b279d671173094c715b0e53c9ea363e
Daniel Pohl <pohl.daniel@freenet.de>
2026-05-22 21:38:27 +0200

    fix

--- src/assets/wasm/16PrimitivePicking/States/PrimitivePicking.cpp
+++ src/assets/wasm/16PrimitivePicking/States/PrimitivePicking.cpp
@@ -78,29 +78,19 @@ PrimitivePicking::PrimitivePicking(StateMachine& machine) : State(machine, State
 	m_debugBindGroup = createDebugBindGroup();
 
   
-	std::cout << "1111111111#" << std::endl;
+	std::cout << "1111111111q" << std::endl;
 	for (unsigned int k = 0; k < m_teapot.getMesh()->getIndexBuffer().size() / 3u; ++k) {
 		unsigned int index0 = m_teapot.getMesh()->getIndexBuffer()[k * 3];
 		unsigned int index1 = m_teapot.getMesh()->getIndexBuffer()[k * 3 + 1];
 		unsigned int index2 = m_teapot.getMesh()->getIndexBuffer()[k * 3 + 2];
 
-		m_vertices.push_back({ m_teapot.getMesh()->getVertexBuffer()[index0 * 6 + 0] , m_teapot.getMesh()->getVertexBuffer()[index0 * 6 + 1] , m_teapot.getMesh()->getVertexBuffer()[index0 * 6 + 2],
-							m_teapot.getMesh()->getVertexBuffer()[index0 * 6 + 3] , m_teapot.getMesh()->getVertexBuffer()[index0 * 6 + 4] , m_teapot.getMesh()->getVertexBuffer()[index0 * 6 + 5],
-							k});
-
-		m_vertices.push_back({ m_teapot.getMesh()->getVertexBuffer()[index1 * 6 + 0] , m_teapot.getMesh()->getVertexBuffer()[index1 * 6 + 1] , m_teapot.getMesh()->getVertexBuffer()[index1 * 6 + 2],
-							m_teapot.getMesh()->getVertexBuffer()[index1 * 6 + 3] , m_teapot.getMesh()->getVertexBuffer()[index1 * 6 + 4] , m_teapot.getMesh()->getVertexBuffer()[index1 * 6 + 5],
-							k });
-
-		m_vertices.push_back({ m_teapot.getMesh()->getVertexBuffer()[index2 * 6 + 0] , m_teapot.getMesh()->getVertexBuffer()[index2 * 6 + 1] , m_teapot.getMesh()->getVertexBuffer()[index2 * 6 + 2],
-							m_teapot.getMesh()->getVertexBuffer()[index2 * 6 + 3] , m_teapot.getMesh()->getVertexBuffer()[index2 * 6 + 4] , m_teapot.getMesh()->getVertexBuffer()[index2 * 6 + 5],
-							k });	
+		
 
 		m_indices.push_back(k * 3); m_indices.push_back(k * 3 + 1); m_indices.push_back(k * 3 + 2);
-		if(k == 100)
+		if(k == 10)
 			break;
 	}
-	std::cout << "22222222#" << std::endl;
+	std::cout << "22222222q" << std::endl;
 	m_vertexBuffer.createBuffer(reinterpret_cast<const void*>(m_vertices.data()), sizeof(Vertex) * m_vertices.size(), WGPUBufferUsage_Vertex | WGPUBufferUsage_Storage);
 	m_indexBuffer.createBuffer(reinterpret_cast<const void*>(m_indices.data()), sizeof(unsigned int) * m_indices.size(), WGPUBufferUsage_Index | WGPUBufferUsage_Storage);
 
